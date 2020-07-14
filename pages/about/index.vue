2ed119861a1da20aa20a0d01849093aa7ef1e9d7
altynbek07 <altynbek.kazezov.97@gmail.com>
2020-07-14 18:03:42 +0500

    fix: about, posts and posts view pages

---
+++ pages/about/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="about-page">
+    <h1>This is title</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis expedita impedit incidunt illo reiciendis eveniet obcaecati! Dolore eum magnam, porro at mollitia dolorum. Ullam, maxime. Officiis vel cum hic rerum!</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis expedita impedit incidunt illo reiciendis eveniet obcaecati! Dolore eum magnam, porro at mollitia dolorum. Ullam, maxime. Officiis vel cum hic rerum!</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corporis expedita impedit incidunt illo reiciendis eveniet obcaecati! Dolore eum magnam, porro at mollitia dolorum. Ullam, maxime. Officiis vel cum hic rerum!</p>
+  </div>
+</template>
+<style scoped>
+.about-page {
+  text-align: center;
+  padding: 30px;
+}
+</style>
