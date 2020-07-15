60949146b370130afc113ed35cf23145a1092b3d
altynbek07 <altynbek.kazezov.97@gmail.com>
2020-07-15 20:43:26 +0500

    fix: admin and error layouts

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="admin-page">
     <section class="new-post">
-      <button @click="$router.push('/admin/new-post')">Create Post</button>
+      <AppButton @click="$router.push('/admin/new-post')"
+        >Create Post</AppButton
+      >
     </section>
     <section class="existing-posts">
       <h1>Existing Posts</h1>
@@ -12,10 +14,13 @@
 
 <script>
 import PostList from "@/components/Posts/PostList";
+import AppButton from "@/components/UI/AppButton";
 
 export default {
+  layout: "admin",
   components: {
-    PostList
+    PostList,
+    AppButton
   }
 };
 </script>
